20815ed4e2858fabcd0f923ac3dbee938b06753a
0oM4R <omarksm09@gmail.com>
2022-08-05 16:37:09 +0200

    add cypress integration

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
+module.exports = {
+  
+  // The rest of the Cypress config options go here...
+}
 
 module.exports = defineConfig({
+  projectId: "mcygbp",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
