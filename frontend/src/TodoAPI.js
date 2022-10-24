830ffcc117e804d90705fd18d01206e2d0ebaf0c
kassem <omarksm09@gmail.com>
2022-10-24 18:16:53 +0200

    add env values for frontend

--- frontend/src/TodoAPI.js
+++ frontend/src/TodoAPI.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 const baseUrl = BACKEND_URL
-console.log(baseUrl )
+console.log(baseUrl)
 export default class TodoApi{
      static async getAll(){
           try {
-               const res = await axios.get(baseUrl+"todo/all")
+               const res = await axios.get(baseUrl+"/api/todo/all")
                return res.data
           }catch(err) {
                console.log(err)
           }
      }
      static async updateTodo(id,task,done) {
-          await axios.put(baseUrl +"todo/"+"?taskId="+id,{
+          await axios.put(baseUrl +"/api/todo/"+"?taskId="+id,{
                task:task,
                done:done
           }).then(function(res) {
@@ -22,14 +22,14 @@ export default class TodoApi{
           })
      }
      static async deleteTodo(id) {
-          await axios.delete(baseUrl +"todo/"+"?taskId="+id).then(function(res) {
+          await axios.delete(baseUrl +"/api/todo/"+"?taskId="+id).then(function(res) {
                console.log(res)
           }).catch((err)=>{
                console.log(err)
           })
      }
      static async newTodo(task) {
-          await axios.post(baseUrl +"todo",{
+          await axios.post(baseUrl +"/api/todo",{
                task:task,
           }).then(function(res) {
                console.log(res)
